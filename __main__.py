9a6b8c7101905df65e108830f234af5a8e03e911
elric <devwerst@gmail.com>
2023-12-26 09:38:45 -0500

    Added access  keys to default data

--- __main__.py
+++ __main__.py
@@ -1,7 +1,8 @@
 import DefaultData.Scripts.famplan as famplan
 import DefaultData.Scripts.tuberculosis as tuberculosis
 from SpectrumCommon.Const.FP import *
-
+from AvenirCommon.Keys import store_keys_in_env
+store_keys_in_env()
 # famplan.create_famplan_db('V5', FP_Interpolated) 
 # famplan.create_famplan_db('V3', FP_Uninterpolated)
 # famplan.create_famplan_effectiveness('V2')
@@ -12,5 +13,5 @@ from SpectrumCommon.Const.FP import *
 # tuberculosis.create_TB_fort_input('V2')
 # tuberculosis.upload_tb_fort_inputs_db('V2')
 
-# tuberculosis.create_TB_fort_outputs('V3')
-tuberculosis.upload_tb_fort_outputs_db('V3')
+tuberculosis.create_TB_fort_outputs('V3')
+# tuberculosis.upload_tb_fort_outputs_db('V3')
