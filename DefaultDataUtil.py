2c06997dc55f3fbe96be02016f8d78d6161aae09
Mark Roman <mroman@avenirhealth.org>
2024-01-04 16:20:26 -0500

    Added methods for returning paths to source and JSON data.

--- DefaultDataUtil.py
+++ DefaultDataUtil.py
@@ -2,6 +2,8 @@ import os
 from AvenirCommon.Logger import log
 from AvenirCommon.Database import GB_upload_json, GB_get_db_json, GB_upload_file
 
+from SpectrumCommon.Const.GB import *
+
 
 country_dir = 'country'
 
@@ -33,3 +35,9 @@ def uploadFilesInDir(container, directory, version, pathMod = ''):
                 fileName = os.path.join(root.replace(directory, '').replace('\\', ''), file)    # strips root directory off of root, so subdirectory is added.  
                                                                                                 # might need recursive strategy if you want to go more than one directory deep
                 GB_upload_file(connection, container, os.path.join(pathMod, fileName), os.path.join(root, file))
+
+def get_source_data_path(mod_ID = int):
+    return os.getcwd() + '\DefaultData\SourceData\\' + GB_CONTAINERS[mod_ID]
+
+def get_JSON_data_path(mod_ID = int, file_name = ''):
+    return os.getcwd() + '\DefaultData\JSONData\\' + GB_CONTAINERS[mod_ID]
