e7026e6b8d7e74e760d60b1557333e220539e4c7
Mark Roman <mroman@avenirhealth.org>
2024-06-17 13:34:05 -0400

    Phase and intensity database is now connected to costing subsections for PPLI.

--- Scripts/programmecosting/PCUploadPPLIPhaseDB.py
+++ Scripts/programmecosting/PCUploadPPLIPhaseDB.py
@@ -5,6 +5,7 @@ from openpyxl import load_workbook
 
 # from AvenirCommon.Database import GB_upload_file
 from AvenirCommon.Logger import log
+from AvenirCommon.Util import GBRange
 
 import DefaultData.DefaultDataUtil as ddu
 
@@ -18,8 +19,14 @@ import SpectrumCommon.Const.PC.PCDatabaseKeys as pcdbk
 PC_ISO_NUMERIC_COUNTRY_CODE = '<ISO Numeric Country Code>'  
 PC_ISO_ALPHA_3_COUNTRY_CODE = '<ISO Alpha-3 Country Code>'
 PC_COUNTRY_NAME             = '<Country Name>'
-PC_PHASE_INTENSITY_ROW_ID   = '<Phase and Intensity Row ID>'
-PC_PHASE_INTENSITY_ROW_NAME = '<Phase and Intensity Row Name>'
+PC_AREA_TYPE_ID             = '<Area Type ID>'  
+PC_AREA_TYPE_NAME           = '<Area Type Name>'   
+PC_AREA_ID                  = '<Area ID>'  
+PC_AREA_NAME                = '<Area Name>'   
+PC_COSTING_SECT_ID          = '<Costing Section ID>'    
+PC_COSTING_SECT_NAME        = '<Costing Section Name>' 
+PC_COSTING_SUBSECT_ID       = '<Costing Subsection ID>'       
+PC_COSTING_SUBSECT_NAME     = '<Costing Subsection Name>' 
 PC_PHASE_ID                 = '<Phase ID>'
 PC_PHASE_NAME               = '<Phase Name>'
 
@@ -31,24 +38,53 @@ def create_PPLI_phase_DB_PC(version = str):
     wb = load_workbook(ddu.get_source_data_path(gbc.GB_PC) + '\PCModData.xlsx')
     sheet = wb[pcc.PC_PPLI_PHASE_DB_NAME]
 
+    # first row of intervention data after col descriptions and col tags
+    tag_row = 1
+    first_data_row = 2
+    first_data_col = 1
     num_rows = sheet.max_row
-    # num_cols = sheet.max_column
+    num_cols = sheet.max_column
 
-    # Establish tag columns.
-    col = 1
-    #iso_numeric_country_code_col = col
-    col += 1    
-    #country_name_col = col
-    col += 1
-    iso_alpha_3_country_code_col = col
-    col += 1
-    phase_intensity_row_ID_col = col
-    col += 1
-    # phase_intensity_row_name_col = col
-    col += 1
-    phase_ID_col = col
-    # col += 1
-    # phase_ID_name_col = col
+    iso_alpha_3_country_code_col = gbc.GB_NOT_FOUND
+    area_type_ID_col = gbc.GB_NOT_FOUND
+    area_ID_col = gbc.GB_NOT_FOUND
+    # area_name_col = gbc.GB_NOT_FOUND
+    costing_sect_ID_col = gbc.GB_NOT_FOUND
+    # costing_sect_name_col = gbc.GB_NOT_FOUND
+    # costing_sect_str_const_col = gbc.GB_NOT_FOUND
+    costing_subsect_ID_col = gbc.GB_NOT_FOUND
+    # costing_subsect_name_col = gbc.GB_NOT_FOUND
+    phase_ID_col = gbc.GB_NOT_FOUND
+
+    for c in GBRange(first_data_col, num_cols):
+        tag = sheet.cell(tag_row, c).value
+
+        if tag == PC_ISO_ALPHA_3_COUNTRY_CODE:
+            iso_alpha_3_country_code_col = c
+
+        elif tag == PC_AREA_TYPE_ID:
+            area_type_ID_col = c
+
+        elif tag == PC_AREA_ID:
+            area_ID_col = c
+
+        # elif tag == PC_AREA_NAME:
+        #     area_name_col = c
+
+        elif tag == PC_COSTING_SECT_ID:
+            costing_sect_ID_col = c
+
+        # elif tag == PC_COSTING_SECT_NAME:
+        #     costing_sect_name_col = c
+
+        elif tag == PC_COSTING_SUBSECT_ID:
+            costing_subsect_ID_col = c
+
+        # elif tag == PC_COSTING_SUBSECT_NAME:
+        #     costing_subsect_name_col = c
+
+        elif tag == PC_PHASE_ID:
+            phase_ID_col = c
 
     row = 2
     first_data_row = row
@@ -69,8 +105,11 @@ def create_PPLI_phase_DB_PC(version = str):
             curr_country_alpha_3 = row_country_alpha_3
 
         phase_intensity_rec = {
-            pcdbk.PC_PHASE_INTENSITY_ROW_ID_KEY_PPLIPDB : row[phase_intensity_row_ID_col - 1],
-            pcdbk.PC_PHASE_ID_KEY_PPLIPDB               : row[phase_ID_col - 1]
+            pcdbk.PC_AREA_TYPE_ID_KEY_PPLIPDB           : row[area_type_ID_col - 1],
+            pcdbk.PC_AREA_ID_KEY_PPLIPDB                : row[area_ID_col - 1],
+            pcdbk.PC_COSTING_SECT_ID_KEY_PPLIPDB        : row[costing_sect_ID_col - 1],
+            pcdbk.PC_COSTING_SUBSECT_ID_KEY_PPLIPDB     : row[costing_subsect_ID_col - 1],
+            pcdbk.PC_PHASE_ID_KEY_PPLIPDB               : row[phase_ID_col - 1],
         }
         country_dict[pcdbk.PC_PHASE_INTENSITY_RECORDS].append(phase_intensity_rec)
 
